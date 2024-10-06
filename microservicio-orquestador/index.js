b319239fd3c6d22795dbb3dd7036c741b37e5e47
zamir23lm <76379576@pronabec.edu.pe>
2024-10-07 00:10:28 +0200

    agregue dos endpoints mas a el orquestador

--- microservicio-orquestador/index.js
+++ microservicio-orquestador/index.js
@@ -80,6 +80,59 @@ app.post('/api/orquestador/producto', async(req, res) => {
 
 });
 
+app.delete('/api/orquestador/categoria/eliminar/', async(req, res)=>{
+    const{adminId, categoriaId} = req.body;
+    try{
+        const adminResponse = await axios.get(`http://localhost:5000/admin/${adminId}`);
+        if(adminResponse.status === 200){
+            const categoriaResponse = await axios.delete(`http://localhost:8081/api/categoria/${categoriaId}`)
+            res.status(categoriaResponse.status).json({
+                message:"Categoria elimianda correctamente",
+                data:categoriaResponse.data
+            });
+        }else{
+            res.status(404).json({message:'Adminitrador no encontrado'});
+        }
+    }
+    catch(error){
+        if(error.response){
+            if(error.response.status ===404){
+                res.status(404).json({message:'Administrador no encontrado'});
+            }else{
+                res.status(error.response.status).json({message:'Error aleliminar la categoria', error: error.message});
+            }
+        }else{
+            res.status(500).json({message:'Error interno del del microservicio orquestador'});
+        }
+    }
+});
+
+app.delete('/api/orquestador/producto/eliminar/', async(req, res)=>{
+    const{adminId, productoId} = req.body;
+    try{
+        const adminResponse = await axios.get(`http://localhost:5000/admin/${adminId}`);
+        if(adminResponse.status === 200){
+            const productoResponse = await axios.delete(`http://localhost:8081/api/producto/${productoId}`)
+            res.status(productoResponse.status).json({
+                message:"Producto elimiando correctamente",
+                data:productoResponse.data
+            });
+        }else{
+            res.status(404).json({message:'Adminitrador no encontrado'});
+        }
+    }
+    catch(error){
+        if(error.response){
+            if(error.response.status ===404){
+                res.status(404).json({message:'Administrador no encontrado'});
+            }else{
+                res.status(error.response.status).json({message:'Error aleliminar el producto', error: error.message});
+            }
+        }else{
+            res.status(500).json({message:'Error interno del del microservicio orquestador'});
+        }
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Orquestador de microservicios escuchando en el puerto ${PORT}`);
